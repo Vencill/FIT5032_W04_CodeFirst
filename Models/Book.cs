516645e6a3760fb0b82e0bda82b661c853165bdf
Vencill <wliu0028@student.monash.edu>
2019-08-25 19:36:24 +1000

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_W04_CodeFirst.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public virtual Publisher Publisher { get; set; }
+
+    }
+}
